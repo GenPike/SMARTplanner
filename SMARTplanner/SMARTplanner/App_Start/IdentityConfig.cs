fa2290323476c08b3d2637fd598563f2bb663f8f
GenPike <olexis552@gmail.com>
2016-02-28 21:06:32 +0200

    replace default dbContext and applicationUser classes

--- SMARTplanner/SMARTplanner/App_Start/IdentityConfig.cs
+++ SMARTplanner/SMARTplanner/App_Start/IdentityConfig.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using SMARTplanner.Models;
+using SMARTplanner.Data.Exact;
 
 namespace SMARTplanner
 {
--- SMARTplanner/SMARTplanner/App_Start/Startup.Auth.cs
+++ SMARTplanner/SMARTplanner/App_Start/Startup.Auth.cs
@@ -5,7 +5,7 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
-using SMARTplanner.Models;
+using SMARTplanner.Data.Exact;
 
 namespace SMARTplanner
 {
