03b77bed9775630d5337894302faea0600a14815
GenPike <olexis552@gmail.com>
2016-03-06 01:08:42 +0200

    add logic to IssueService

--- SMARTplanner/SMARTplanner.Logic/Contracts/IIssueService.cs
+++ SMARTplanner/SMARTplanner.Logic/Contracts/IIssueService.cs
@@ -6,9 +6,8 @@ namespace SMARTplanner.Logic.Contracts
 {
     public interface IIssueService
     {
-        IEnumerable<Issue> GetIssuesPaged(string userId, int page = 1, int pageSize = 10);
+        IEnumerable<Issue> GetIssuesPaged(string userId, string summaryPart = null, int page = 1, int pageSize = 10);
         Issue GetIssue(long issueId, string userId);
-        Issue GetIssue(string summaryPart, string userId);
         Issue GetIssue(int trackingNumber, string userId);
         void AddIssue(Issue issue);
         void UpdateIssue(Issue issue, string userId);
--- SMARTplanner/SMARTplanner.Logic/Exact/IssueService.cs
+++ SMARTplanner/SMARTplanner.Logic/Exact/IssueService.cs
@@ -1,48 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using SMARTplanner.Data.Contracts;
 using SMARTplanner.Entities.Domain;
+using SMARTplanner.Logic.Business;
 using SMARTplanner.Logic.Contracts;
 
 namespace SMARTplanner.Logic.Exact
 {
     public class IssueService : IIssueService
     {
-        private ISmartPlannerContext _context;
+        private readonly ISmartPlannerContext _context;
 
         public IssueService(ISmartPlannerContext ctx)
         {
             _context = ctx;
         }
 
-        public IEnumerable<Issue> GetIssuesPaged(string userId, int page = 1, int pageSize = 10)
+        public IEnumerable<Issue> GetIssuesPaged(string userId, string summaryPart = null, int page = 1, int pageSize = 10)
         {
-            throw new NotImplementedException();
+            if (!Inspector.IsValidPageSize(pageSize)) return null;
+
+            //get accessible issues
+            var accessibleIssues = GetAccessibleIssues(userId);
+
+            //filter issues
+            if (summaryPart != null)
+            {
+                accessibleIssues = accessibleIssues
+                    .Where(i => i.Summary.Contains(summaryPart.ToLower()));
+            }
+
+            return accessibleIssues
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize);
         }
+
+        #region AccessHelpers
+
+        private IEnumerable<Issue> GetAccessibleIssues(string userId)
+        {
+            return _context.ProjectUserAccesses
+                .Where(pu => pu.UserId.Equals(userId))
+                .SelectMany(pu => pu.Project.Issues);
+        } 
+
+        #endregion
 
         public Issue GetIssue(long issueId, string userId)
         {
-            throw new NotImplementedException();
-        }
-
-        public Issue GetIssue(string summaryPart, string userId)
-        {
-            throw new NotImplementedException();
+            return GetAccessibleIssues(userId)
+                .SingleOrDefault(i => i.Id == issueId);
         }
 
         public Issue GetIssue(int trackingNumber, string userId)
         {
-            throw new NotImplementedException();
+            return GetAccessibleIssues(userId)
+                .FirstOrDefault(i => i.IssueTrackingNumber == trackingNumber);
         }
 
         public void AddIssue(Issue issue)
         {
-            throw new NotImplementedException();
+            if (issue != null)
+            {
+                //check user access to the project
+                var projUserRef = _context.ProjectUserAccesses
+                    .SingleOrDefault(pu => pu.ProjectId == issue.ProjectId &&
+                        pu.UserId.Equals(issue.CreatorId));
+
+                if (!Inspector.CanUserUpdateProject(projUserRef)) return;
+
+                _context.Issues.Add(issue);
+                _context.SaveChanges();
+            }
         }
 
         public void UpdateIssue(Issue issue, string userId)
         {
-            throw new NotImplementedException();
+            if (issue != null)
+            {
+                var issueToUpdate = _context.Issues
+                    .SingleOrDefault(i => i.Id == issue.Id);
+
+                if (issueToUpdate != null)
+                {
+                    //check user access to the project
+                    var projUserRef = _context.ProjectUserAccesses
+                        .SingleOrDefault(pu => pu.ProjectId == issueToUpdate.ProjectId &&
+                                               pu.UserId.Equals(userId));
+
+                    if (!Inspector.CanUserUpdateProject(projUserRef)) return;
+
+                    _context.Entry(issueToUpdate).CurrentValues.SetValues(issue);
+                    _context.SaveChanges();
+                }
+            }
         }
     }
 }
