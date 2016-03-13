db88366ae330be1a8810a1a5458cdebf321ede70
GenPike <olexis552@gmail.com>
2016-03-14 01:43:47 +0200

    add TestContext for unit testing

---
+++ SMARTplanner/SMARTplanner.Tests/TestEntities/ExactTestDbSets.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using SMARTplanner.Entities.Domain;
+
+namespace SMARTplanner.Tests.TestEntities
+{
+    class TestProjectDbSet : TestDbSet<Project>
+    {
+        public override Project Find(params object[] keyValues)
+        {
+            return this.SingleOrDefault(proj => proj.Id == (long)keyValues.Single());
+        }
+    }
+
+    class TestProjectUserAccessDbSet : TestDbSet<ProjectUserAccess>
+    {
+        public override ProjectUserAccess Find(params object[] keyValues)
+        {
+            return this.SingleOrDefault(pu => pu.ProjectId == (long)keyValues[0] &&
+                pu.UserId.Equals((string)keyValues[1]));
+        }
+    }
+
+    class TestIssueDbSet : TestDbSet<Issue>
+    {
+        public override Issue Find(params object[] keyValues)
+        {
+            return this.SingleOrDefault(i => i.Id == (long)keyValues.Single());
+        }
+    }
+
+    class TestWorkItemDbSet : TestDbSet<WorkItem>
+    {
+        public override WorkItem Find(params object[] keyValues)
+        {
+            return this.SingleOrDefault(w => w.Id == (long)keyValues.Single());
+        }
+    }
+
+    class TestReportDbSet : TestDbSet<Report>
+    {
+        public override Report Find(params object[] keyValues)
+        {
+            return this.SingleOrDefault(r => r.Id == (long)keyValues.Single());
+        }
+    }
+}
---
+++ SMARTplanner/SMARTplanner.Tests/TestEntities/TestSmartPlannerContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using SMARTplanner.Data.Contracts;
+using SMARTplanner.Entities.Domain;
+
+namespace SMARTplanner.Tests.TestEntities
+{
+    class TestSmartPlannerContext : ISmartPlannerContext
+    {
+        public TestSmartPlannerContext()
+        {
+            Projects = new TestProjectDbSet();
+            ProjectUserAccesses = new TestProjectUserAccessDbSet();
+            Issues = new TestIssueDbSet();
+            WorkItems = new TestWorkItemDbSet();
+            Reports = new TestReportDbSet();
+        }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<ProjectUserAccess> ProjectUserAccesses { get; set; }
+
+        public DbSet<Issue> Issues { get; set; }
+
+        public DbSet<WorkItem> WorkItems { get; set; }
+
+        public DbSet<Report> Reports { get; set; }
+
+        public DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public DbSet<TEntity> Set<TEntity>() where TEntity : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SaveChanges()
+        {
+            return 0;
+        }
+
+        public void Dispose() { }
+    }
+}
