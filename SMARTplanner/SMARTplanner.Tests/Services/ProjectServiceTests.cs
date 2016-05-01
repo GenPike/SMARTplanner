1818c3e4355fbf140c5a6a84c5dd1cc173c0d359
GenPike <olexis552@gmail.com>
2016-05-01 16:24:39 +0300

    Added a few unit tests for ProjectService

---
+++ SMARTplanner/SMARTplanner.Tests/Services/ProjectServiceTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SMARTplanner.Data.Contracts;
+using SMARTplanner.Entities.Domain;
+using SMARTplanner.Logic.Contracts;
+using SMARTplanner.Logic.Exact;
+using SMARTplanner.Tests.TestHelpers;
+
+namespace SMARTplanner.Tests.Services
+{
+    [TestClass]
+    public class ProjectServiceTests
+    {
+        [TestMethod]
+        public void Can_GetProject_By_Id()
+        {
+            //Arrange
+            #region Arrange
+
+            var dbSet = new TestProjectDbSet();
+            var mockContext = new Mock<ISmartPlannerContext>();
+            var mockAccessService = new Mock<IAccessService>();
+
+            var testProject = new Project { Id = 1 };
+            dbSet.Add(testProject);
+            mockContext.Setup(m => m.Projects).Returns(dbSet);
+            mockAccessService.Setup(m => m.GetAccessByProject(It.IsAny<long>(), It.IsAny<string>()))
+                .Returns(new ProjectUserAccess());
+
+            var projectService = new ProjectService(mockContext.Object, mockAccessService.Object);
+
+            #endregion
+
+            //Act
+            var result = projectService.GetProject(testProject.Id, string.Empty);
+
+            //Assert
+            Assert.IsFalse(result.ErrorHandled);
+            Assert.AreEqual(result.TargetObject.Id, testProject.Id);
+        }
+
+        [TestMethod]
+        public void Can_GetProject_By_Name()
+        {
+            //Arrange
+            #region Arrange
+
+            var dbSet = new TestProjectDbSet();
+            var mockContext = new Mock<ISmartPlannerContext>();
+            var mockAccessService = new Mock<IAccessService>();
+
+            var testProject = new Project { Name = "OutlookAddIn" };
+            dbSet.Add(testProject);
+            mockContext.Setup(m => m.Projects).Returns(dbSet);
+            mockAccessService.Setup(m => m.GetAccessByProject(It.IsAny<long>(), It.IsAny<string>()))
+                .Returns(new ProjectUserAccess());
+
+            var projectService = new ProjectService(mockContext.Object, mockAccessService.Object);
+
+            #endregion
+
+            //Act
+            var result = projectService.GetProject(testProject.Name, string.Empty);
+
+            //Assert
+            Assert.IsFalse(result.ErrorHandled);
+            Assert.AreEqual(result.TargetObject.Name, testProject.Name);
+        }
+    }
+}
