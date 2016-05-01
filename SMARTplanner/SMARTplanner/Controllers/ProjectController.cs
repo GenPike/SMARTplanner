8089fd464bfa601d51ac435f721911905c7cbfda
GenPike <olexis552@gmail.com>
2016-05-01 23:25:31 +0300

    Add listItem view for project

---
+++ SMARTplanner/SMARTplanner/Controllers/ProjectController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMARTplanner.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
